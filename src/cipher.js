96dd309776972ad451bc162304ae8a5b0cd43602
jaurinu <jaurinu@gmail.com>
2019-01-17 12:16:07 -0600

    Funcion encode operando

--- src/cipher.js
+++ src/cipher.js
@@ -1,51 +1,54 @@
 window.cipher = {
-//..
-  encode: (offset,mensaje) => {
-   
-//for (let i=0; i<textoEncriptar.length; i++) {
-//    let ascii = textoEncriptar.charCodeAt(i);
-//    console.log (ascii);
-    
-//}
+  //..
+  encode: (offset, mensaje) => {
 
-//let textoffset = ((ascii-65+numeroOffset)%26+65);
-//console.log (textoffset);
+    //for (let i=0; i<textoEncriptar.length; i++) {
+    //    let ascii = textoEncriptar.charCodeAt(i);
+    //    console.log (ascii);
 
-//let ascii = "";
-//let cadenaascii = [];
-//const mensajeCifrado = () => {
-//const arrascii = '';
+    //}
 
-for (let i=0; i<mensaje.length; i++) {
-  let ascii = mensaje.charCodeAt(i);
-  let numOffset = ((ascii-65+parseInt(offset))%26+65);
-  console.log (ascii);
-  console.log (numOffset);
-  //arrascii += numOffset;
-  
-}
+    //let textoffset = ((ascii-65+numeroOffset)%26+65);
+    //console.log (textoffset);
 
+    //let ascii = "";
+    //let cadenaascii = [];
+    //const mensajeCifrado = () => {
+    let palAscii = "";
 
-//console.log (mensajeCifrado);//
-//}
-},
-
-
-
-
-//let textoffset = ((ascii-65+numeroOffset)%26+65);
-//console.log (textoffset);
+    for (let i = 0; i < mensaje.length; i++) {
+      let ascii = mensaje.charCodeAt(i);
+      let numOffset = ((ascii - 65 + parseInt(offset)) % 26 + 65);
+      //console.log (ascii);
+      //console.log (numOffset);
+      //arrascii += numOffset;
+      // palAscii = palAscii + numOffset
+      let cambioLetra = String.fromCharCode(numOffset);
+      palAscii += cambioLetra;
+      
+    }
+    //console.log(palAscii);
+return palAscii;
+    //console.log (mensajeCifrado);//
+    //}
+  },
 
 
 
 
+  //let textoffset = ((ascii-65+numeroOffset)%26+65);
+  //console.log (textoffset);
 
 
 
-//let numeroOffset
-//let textoEncriptar
 
-  
+
+
+
+  //let numeroOffset
+  //let textoEncriptar
+
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 //Declarar evento boton encriptar y funciones 
 //const Cifrado = document.getElementById('botonCifrar')cifrado.addEventListener('click' , ()=>{
 
-document.getElementById('botonCifrar').addEventListener('click',()=>{  
+document.getElementById('botonCifrar').addEventListener('click', () => {
 
-    //Obtener offset y texto a encriptar convertirlo a mayusculas
-    let offset = document.getElementById ('offset').value;
-    let mensaje = document.getElementById ('mensaje').value.toUpperCase();
-    
-    
-    console.log(offset);
-    console.log(mensaje);
-    window.cipher.encode(offset,mensaje);
-   
+  //Obtener offset y texto a encriptar convertirlo a mayusculas
+  let offset = document.getElementById('offset').value;
+  let mensaje = document.getElementById('mensaje').value.toUpperCase();
+
+  window.cipher.encode(offset, mensaje);
+
 })
