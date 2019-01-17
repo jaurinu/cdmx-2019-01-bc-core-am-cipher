f55cc4ae6ca37bd6504ff3ecf2495847cbf56c1e
jaurinu <jaurinu@gmail.com>
2019-01-17 11:28:53 -0600

    Cambios en botones, convertido a codigo ascii y desplazamiento

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,55 @@
 window.cipher = {
 //..
-  encode: () => {},
+  encode: (offset,mensaje) => {
+   
+//for (let i=0; i<textoEncriptar.length; i++) {
+//    let ascii = textoEncriptar.charCodeAt(i);
+//    console.log (ascii);
+    
+//}
+
+//let textoffset = ((ascii-65+numeroOffset)%26+65);
+//console.log (textoffset);
+
+//let ascii = "";
+//let cadenaascii = [];
+//const mensajeCifrado = () => {
+//const arrascii = '';
+
+for (let i=0; i<mensaje.length; i++) {
+  let ascii = mensaje.charCodeAt(i);
+  let numOffset = ((ascii-65+parseInt(offset))%26+65);
+  console.log (ascii);
+  console.log (numOffset);
+  //arrascii += numOffset;
+  
+}
+
+
+//console.log (mensajeCifrado);//
+//}
+},
+
+
+
+
+//let textoffset = ((ascii-65+numeroOffset)%26+65);
+//console.log (textoffset);
+
+
+
+
+
+
+
+//let numeroOffset
+//let textoEncriptar
+
+  
+
+
+
+
+
   decode: () => {}
 };
