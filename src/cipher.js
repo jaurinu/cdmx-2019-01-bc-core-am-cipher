d4cec95e54211e3d4eaad9b06094ba6f9faa920e
jaurinu <jaurinu@gmail.com>
2019-01-17 22:16:57 -0600

    Funcion decode operando

--- src/cipher.js
+++ src/cipher.js
@@ -2,57 +2,40 @@ window.cipher = {
   //..
   encode: (offset, mensaje) => {
 
-    //for (let i=0; i<textoEncriptar.length; i++) {
-    //    let ascii = textoEncriptar.charCodeAt(i);
-    //    console.log (ascii);
-
-    //}
-
-    //let textoffset = ((ascii-65+numeroOffset)%26+65);
-    //console.log (textoffset);
-
-    //let ascii = "";
-    //let cadenaascii = [];
-    //const mensajeCifrado = () => {
     let palAscii = "";
 
     for (let i = 0; i < mensaje.length; i++) {
       let ascii = mensaje.charCodeAt(i);
+        //console.log (ascii);
       let numOffset = ((ascii - 65 + parseInt(offset)) % 26 + 65);
-      //console.log (ascii);
       //console.log (numOffset);
-      //arrascii += numOffset;
-      // palAscii = palAscii + numOffset
       let cambioLetra = String.fromCharCode(numOffset);
       palAscii += cambioLetra;
-      
     }
-    //console.log(palAscii);
-return palAscii;
-    //console.log (mensajeCifrado);//
-    //}
+    return palAscii;
+    //console.log (palAscii);
   },
 
+  decode: (offset, mensaje) => {
+    let palAscii = "";
+
+    for (let i = 0; i < mensaje.length; i++) {
+      let ascii = mensaje.charCodeAt(i);
+        //console.log (ascii);
+      let numOffset = ((ascii + 65 - parseInt(offset)) % 26 + 65);
+      //console.log (numOffset);
+      let cambioLetra = String.fromCharCode(numOffset);
+      palAscii += cambioLetra;
+    }
+    return palAscii;
+    //console.log (palAscii);
+  }
 
 
 
-  //let textoffset = ((ascii-65+numeroOffset)%26+65);
-  //console.log (textoffset);
 
 
 
 
 
-
-
-  //let numeroOffset
-  //let textoEncriptar
-
-
-
-
-
-
-
-  decode: () => {}
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 //Declarar evento boton encriptar y funciones 
 //const Cifrado = document.getElementById('botonCifrar')cifrado.addEventListener('click' , ()=>{
 
+   //const offset = document.getElementById('offset').value;
+   //const mensaje = document.getElementById('mensaje').value.toUpperCase();
+
+
+
+
 document.getElementById('botonCifrar').addEventListener('click', () => {
 
   //Obtener offset y texto a encriptar convertirlo a mayusculas
@@ -8,5 +14,16 @@ document.getElementById('botonCifrar').addEventListener('click', () => {
   let mensaje = document.getElementById('mensaje').value.toUpperCase();
 
   window.cipher.encode(offset, mensaje);
+})
 
+document.getElementById('botonDescifrar').addEventListener('click', () => {
+
+  //Obtener offset y texto a encriptar convertirlo a mayusculas
+  let offset = document.getElementById('offset').value;
+  let mensaje = document.getElementById('mensaje').value.toUpperCase();
+
+  //console.log(offset);
+  //console.log(mensaje);
+
+  window.cipher.decode(offset, mensaje);
 })
