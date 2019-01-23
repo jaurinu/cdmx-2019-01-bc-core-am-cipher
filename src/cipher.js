b9ba14b44c3bd461fad32c16ab6c560aa1fbbb5a
jaurinu <jaurinu@gmail.com>
2019-01-23 10:50:10 -0600

    Imprime resultado

--- src/cipher.js
+++ src/cipher.js
@@ -2,33 +2,36 @@ window.cipher = {
   //..
   encode: (offset, mensaje) => {
 
-    let palAscii = "";
+   let palAscii = "";
 
     for (let i = 0; i < mensaje.length; i++) {
       let ascii = mensaje.charCodeAt(i);
-        //console.log (ascii);
+      //console.log (ascii);
       let numOffset = ((ascii - 65 + parseInt(offset)) % 26 + 65);
       //console.log (numOffset);
       let cambioLetra = String.fromCharCode(numOffset);
       palAscii += cambioLetra;
     }
-    return palAscii;
-    //console.log (palAscii);
-  },
+    //console.log (palAscii); 
+   return palAscii;
+    },
+
+    
 
   decode: (offset, mensaje) => {
     let palAscii = "";
 
     for (let i = 0; i < mensaje.length; i++) {
       let ascii = mensaje.charCodeAt(i);
-        //console.log (ascii);
+      //console.log (ascii);
       let numOffset = ((ascii + 65 - parseInt(offset)) % 26 + 65);
       //console.log (numOffset);
       let cambioLetra = String.fromCharCode(numOffset);
       palAscii += cambioLetra;
     }
-    return palAscii;
     //console.log (palAscii);
+    return palAscii;
+    
   }
 
 
