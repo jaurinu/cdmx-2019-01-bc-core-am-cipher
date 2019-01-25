5f0a3b30c78b0d4f353598ae2b47eb3a4dbb2961
jaurinu <jaurinu@gmail.com>
2019-01-24 21:44:05 -0600

    Css corregido. Index js variables declaradas de manera global. HTML corregido

--- src/cipher.js
+++ src/cipher.js
@@ -4,7 +4,7 @@ window.cipher = {
    let palAscii = "";
     for (let i = 0; i < mensaje.length; i++) {
       let ascii = mensaje.charCodeAt(i);
-      let numOffset = ((ascii - 65 + parseInt(offset)) % 26 + 65);
+      let numOffset = (ascii - 65 + parseInt(offset)) % 26 + 65;
       let cambioLetra = String.fromCharCode(numOffset);
       palAscii += cambioLetra;
     }
@@ -15,18 +15,11 @@ window.cipher = {
     let palAscii = "";
     for (let i = 0; i < mensaje.length; i++) {
       let ascii = mensaje.charCodeAt(i);
-      let numOffset = ((ascii + 65 - parseInt(offset)) % 26 + 65);
+      let numOffset = (ascii + 65 - parseInt(offset)) % 26 + 65;
       let cambioLetra = String.fromCharCode(numOffset);
       palAscii += cambioLetra;
     }
     return palAscii;
   }
-
-
-
-
-
-
-
 
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 //Declarar evento boton encriptar y funciones 
 
+let offset = document.getElementById('offset');
+let mensaje = document.getElementById('mensaje');
 const resCifrado = document.getElementById('resCifrado');
 
 document.getElementById('botonCifrar').addEventListener('click', () => {
-  let offset = document.getElementById('offset').value;
-  let mensaje = document.getElementById('mensaje').value.toUpperCase();
+  offset = offset.value;
+  mensaje = mensaje.value.toUpperCase();
   let resultado = window.cipher.encode(offset, mensaje);
-  resCifrado.innerHTML = resultado;
+  resCifrado.value = resultado;
  })
 
 document.getElementById('botonDescifrar').addEventListener('click', () => {
-  let offset = document.getElementById('offset').value;
-  let mensaje = document.getElementById('mensaje').value.toUpperCase();
+  offset = offset.value;
+  mensaje = mensaje.value.toUpperCase();
   let resultado = window.cipher.decode(offset, mensaje);
- resCifrado.innerHTML = resultado;
+ resCifrado.value = resultado;
 })
